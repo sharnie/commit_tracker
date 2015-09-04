a13eebade001284e5df3d1527a0f9d7b8e478ddd
Sharnie Ivery <sharnieivery@gmail.com>
2015-09-04 07:53:26 -0400

    initial commit

---
+++ commit_tracker.rb
@@ -0,0 +1,40 @@
+require 'nokogiri'
+require 'open-uri'
+
+# usernames = [
+#   'brianknight10',
+#   'annekagoss',
+#   'bethannezink',
+#   'DanFerrer',
+#   'antropova',
+#   'himedlooff',
+#   'sharnie',
+#   'CarpeDN',
+#   'aromayne',
+#   'tadasv',
+#   'rvangundy',
+#   'samueldowens'
+# ]
+
+usernames = {
+  "brianknight10" => "Brian",
+  "annekagoss" => "Anneka",
+  "bethannezink" => "Bethanne",
+  "DanFerrer" => "Dan",
+  "antropova" => "Masha",
+  "himedlooff" => "Mike",
+  "sharnie" => "Sharnie",
+  "CarpeDN" => "Dien"
+}
+
+usernames.each do |username, real_name|
+  begin
+    doc = Nokogiri::HTML(open("https://www.github.com/#{username}"))
+    color = doc.css("#contributions-calendar [data-date=\"#{Date.today.prev_day}\"]").attr("fill")
+    committed = color.to_s == "#eeeeee" ? "No commits" : "Committed"
+    puts "#{username} (#{real_name}): #{committed}"
+    # `open https://github.com/#{username}` if committed === "No commits"
+  rescue
+    puts "Invalid username: #{username}"
+  end
+end
